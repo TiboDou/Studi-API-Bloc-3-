918231e19009b855010143db847bce998a89675e
TiboDou <thibault.douchin@gmail.com>
2023-05-19 15:22:27 +0200

    Added cors policy

---
+++ src/main/java/com/bloc3/vitrine_produits_promo/WebConfig.java
@@ -0,0 +1,26 @@
+package com.bloc3.vitrine_produits_promo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class WebConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebConfig.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/produits").allowedOrigins("https://studi-bloc3-front-td.herokuapp.com");
+            }
+        };
+    }
+
+}
