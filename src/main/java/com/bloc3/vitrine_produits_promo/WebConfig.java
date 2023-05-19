a4ab898cd9eb3840db4c7b8aec2765c9db58e8d9
TiboDou <thibault.douchin@gmail.com>
2023-05-19 16:04:47 +0200

    Some update

--- src/main/java/com/bloc3/vitrine_produits_promo/WebConfig.java
+++ src/main/java/com/bloc3/vitrine_produits_promo/WebConfig.java
@@ -1,6 +1,5 @@
 package com.bloc3.vitrine_produits_promo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
