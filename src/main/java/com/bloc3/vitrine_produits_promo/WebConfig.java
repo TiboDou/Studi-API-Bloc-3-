17f1bfca779a748c324d2c1046f40cfac216b8f9
TiboDou <thibault.douchin@gmail.com>
2023-05-19 15:37:41 +0200

    Added cors policy

--- src/main/java/com/bloc3/vitrine_produits_promo/WebConfig.java
+++ src/main/java/com/bloc3/vitrine_produits_promo/WebConfig.java
@@ -2,9 +2,11 @@ package com.bloc3.vitrine_produits_promo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig {
 
     public static void main(String[] args) {
