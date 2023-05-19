42f3592a052907433b1f79cf1ed4f1d718882afd
TiboDou <thibault.douchin@gmail.com>
2023-05-19 16:53:48 +0200

    Ajout def classe produit swagger

--- src/main/java/com/bloc3/vitrine_produits_promo/REST/ProduitsController.java
+++ src/main/java/com/bloc3/vitrine_produits_promo/REST/ProduitsController.java
@@ -3,6 +3,11 @@ package com.bloc3.vitrine_produits_promo.REST;
 import com.bloc3.vitrine_produits_promo.Models.Produits;
 import com.bloc3.vitrine_produits_promo.REST.Services.ProduitsServices;
 import com.bloc3.vitrine_produits_promo.util.SiExiste;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -14,25 +19,26 @@ import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/produits")
-//@CrossOrigin(origins ="https://studi-bloc3-front-td.herokuapp.com")
+@Tag(name = "Produits", description = "Endpoint pour la gestion des produits")
 public class ProduitsController {
 
     @Autowired //
     private ProduitsServices prodService;
 
     @GetMapping
+    @Operation(summary = "Récupère tous les produits")
     public List<Produits> selectProduits() {
         List<Produits> produits = prodService.selectProduits();
-
         for (Produits produit : produits) {
             Link selfLink = WebMvcLinkBuilder.linkTo(ProduitsController.class).slash(produit.getNo_produit()).withSelfRel();
             produit.add(selfLink);
         }
-
         return prodService.selectProduits();
     }
 
     @GetMapping("/{no_produit}")
+    @Operation(summary = "Permet de récupérer un produit via son ID")
+    @ApiResponse(responseCode = "200", description = "Le produit a été trouvé", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Produits.class)))
     public Produits findById(@PathVariable("no_produit") int no_produit) {
         Produits reponse =  prodService.findById(no_produit);
         SiExiste.checkFound(reponse);
@@ -41,13 +47,14 @@ public class ProduitsController {
 
 
     @PostMapping
+    @Operation(summary = "Permet de créer un nouveau produit")
     @ResponseStatus(code = HttpStatus.CREATED)
     public int create(@RequestBody Produits produit) {
-        
         return prodService.create(produit);
     }
 
     @PutMapping("/{no_produit}")
+    @Operation(summary = "Permet de mettre à jouer un produit via son ID")
     @ResponseStatus(code = HttpStatus.OK)
     public void update(@PathVariable("no_produit") int no_produit, @RequestBody Produits produit) {
         SiExiste.checkFound(prodService.findById(no_produit));
@@ -55,6 +62,7 @@ public class ProduitsController {
     }
 
     @PatchMapping("/{no_produit}")
+    @Operation(summary = "Permet de mettre à jour partiellement un produit via son ID")
     @ResponseStatus(code = HttpStatus.OK)
     public void partialUpdate(@PathVariable("no_produit") int no_produit, @RequestBody Map<String, Object> updates) {
         SiExiste.checkFound(prodService.findById(no_produit));
@@ -62,6 +70,7 @@ public class ProduitsController {
     }
 
     @DeleteMapping("/{no_produit}")
+    @Operation(summary = "Permet de supprimer un produit via son ID")
     @ResponseStatus(code = HttpStatus.OK)
     public void deleteById(@PathVariable("no_produit") int no_produit) {
         SiExiste.checkFound(prodService.findById(no_produit)); //Méthode qui permet de savoir si le produit qu'on cherche existe dans la bdd
